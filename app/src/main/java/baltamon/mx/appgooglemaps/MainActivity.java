a6e7d6fa7ad4259f68c56369ecd50a2534604598
Balthair94 <archerbaltazar@gmail.com>
2017-04-18 23:29:43 -0500

    Styles and toolbar added

--- app/src/main/java/baltamon/mx/appgooglemaps/MainActivity.java
+++ app/src/main/java/baltamon/mx/appgooglemaps/MainActivity.java
@@ -1,7 +1,10 @@
 package baltamon.mx.appgooglemaps;
 
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpToolbar();
+    }
+
+    public void setUpToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null){
+            actionBar.setTitle("Google Maps");
+        }
     }
 }
