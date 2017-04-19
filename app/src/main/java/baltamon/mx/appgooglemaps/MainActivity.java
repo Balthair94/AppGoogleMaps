cb7d8aa1cd2dc4c5c80b0869f6f1317645e60dad
Balthair94 <archerbaltazar@gmail.com>
2017-04-18 23:58:46 -0500

    Show current location in the map added

--- app/src/main/java/baltamon/mx/appgooglemaps/MainActivity.java
+++ app/src/main/java/baltamon/mx/appgooglemaps/MainActivity.java
@@ -1,6 +1,10 @@
 package baltamon.mx.appgooglemaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +14,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int MY_LOCATION_PERMISSIONS = 01;
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +29,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setUpMap();
     }
 
-    public void setUpMap(){
+    public void setUpMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    public void setUpToolbar(){
+    public void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
 
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setTitle("Google Maps");
         }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        if (googleMap != null)
+            showCurrentLocation();
+    }
 
+    public void showCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //WE CALL THE DIALOGBOX TO REQUEST FOR THE PERMISSIONS
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_PERMISSIONS);
+        }
+        googleMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_LOCATION_PERMISSIONS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    showCurrentLocation();
+                break;
+        }
     }
 }
